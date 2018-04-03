9599ba0118e6b9a8d956647bc68043b4316f74b0
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-04-03 02:06:27 -0300

    6 - Dominando eventos e manipulação de DOM com jQuery  6.5 EDITANDO ITENS

--- 06-DominandoEventosemanipulacaodeDOM/js/todo.js
+++ 06-DominandoEventosemanipulacaodeDOM/js/todo.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
     /* 6.4 REMOVENDO ITENS COM ESTILO */
     function onTarefaDeleClick() {
@@ -9,6 +9,26 @@ $(function(){
     }
 
     $('.tarefa-delete').click(onTarefaDeleClick);
+
+    /* 6.5 EDITANDO ITENS */
+    function onTarefaEditKeydown(event) {
+        if (event.which === 13) {
+            savePendingEdition($(this));
+        }
+    }
+
+    function savePendingEdition(tarefa) {
+        console.log("E aqui vamos salvar nossa tarefa");
+    }
+
+    function onTarefaItemClick() {
+        var text = $(this).children('.tarefa-texto').text();
+        var content = "<input type='text' class='tarefa-edit' value=' " + text + " '>";
+        $(this).html(content);
+        $('.tarefa-edit').keydown(onTarefaEditKeydown);
+
+    }
+
 
 
 });
